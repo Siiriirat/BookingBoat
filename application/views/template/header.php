aba4bbeb56b381799a7e30d9e4b00ef2da53f5cf
Siiriirat <Sirirat405@gmail.com>
2018-06-08 09:12:15 +0700

    update 08/06/2018

---
+++ application/views/template/header.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+  <link rel="stylesheet" href="<?php echo base_url(); ?>/public/css/bootstrap.css">
+</head>
+<body>
