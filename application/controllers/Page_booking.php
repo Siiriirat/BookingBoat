6fabb165e28e32670d920d75a87d4b8f663879dc
Siiriirat <Sirirat405@gmail.com>
2018-06-08 16:34:38 +0700

    controllers

--- application/controllers/Page_booking.php
+++ application/controllers/Page_booking.php
@@ -1,6 +1,5 @@
 <?php
-
-class Page_boat extends CI_Controller{
+class Page_booking extends CI_Controller{
   var $page = 'Type';
   var $name = 'ABC';
   public function __construct()
@@ -8,14 +7,13 @@ class Page_boat extends CI_Controller{
     parent::__construct();
     $this->load->helper('url');
     $this->load->helper('form');
-
   }
   public function index($boat_type, $id)
   {
-    $data['name'] = $this->name;
-    $data['type'] = $boat_type;
-    $this->load->view('template/page.boat.php', $data);
   }
-  
-
+  public function booking(){
+    echo $this->input->post('date');
+    echo $this->input->post('type');
+    echo $this->input->post('name');
+  }
 }
