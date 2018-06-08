a90cf347bf31ee17573be0e018150a3ae3b0d1b4
khunpiny <khun_pin@hotmail.com>
2018-06-08 13:35:43 +0700

    controller

--- application/controllers/Detail.php
+++ application/controllers/Detail.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Booking extends CI_Controller {
+class Detail extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -20,6 +20,9 @@ class Booking extends CI_Controller {
 	 */
 	public function index()
 	{
+    $this->load->view('template/header');
+    $this->load->view('booking');
+    $this->load->view('template/footer');
 	}
 
 }
--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Booking extends CI_Controller {
+class Home extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -20,6 +20,8 @@ class Booking extends CI_Controller {
 	 */
 	public function index()
 	{
+		$this->load->view('template/header');
+		$this->load->view('home');
+		$this->load->view('template/footer');
 	}
-
 }
