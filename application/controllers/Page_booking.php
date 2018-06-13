177cc1283cfb9c55eca7558398acf5d7bef39ee9
Siiriirat <Sirirat405@gmail.com>
2018-06-13 11:51:04 +0700

    13-06-2018/1

--- application/controllers/Page_booking.php
+++ application/controllers/Page_booking.php
@@ -1,11 +1,9 @@
 <?php
 class Page_booking extends CI_Controller{
   public function booking_Boat(){
-    $data = array(
-                  'yacth_id'=>$this->input->get('id'),
-                  'name'=>$this->input->post('name')
-                 );
-            print_r($data["yatch"]);
+    $data['booking'] = $this->input->get('id');
+
+            print_r($data['booking']);
   }
 
 }
